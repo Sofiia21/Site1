d56c609364ed19f459544ba6f67ec69a7ef25e35
Sofiia21 <sayfullina2107@gmail.com>
2023-12-27 22:31:49 +0200

    Initial commit

--- scripts.js
+++ scripts.js
@@ -1,24 +1,22 @@
 $(document).ready(function(){
-    // Инициализация Slick Slider
     $('.slider').slick({
-      autoplay: true,        // Автоматическая прокрутка слайдов
-      autoplaySpeed: 5000,   // Скорость автоматической прокрутки (5000 мс = 5 секунд)
-      dots: true,            // Показывать точки навигации
-      //arrows: true,          // Показывать стрелки навигации
-      adaptiveHeight: true,  // Адаптировать высоту слайдера к высоте текущего слайда
-      //prevArrow: '<button class="slick-prev" aria-label="Previous" type="button">Previous</button>', // Кнопка предыдущего слайда
-      //nextArrow: '<button class="slick-next" aria-label="Next" type="button">Next</button>', // Кнопка следующего слайда
-      responsive: [          // Адаптивные настройки для разных разрешений
-        {
-          breakpoint: 768,   // Настройки для экранов шириной менее 768px
-          settings: {
-            arrows: false    // Скрыть стрелки на маленьких экранах
-          }
+      autoplay: true,       
+      autoplaySpeed: 5000,   
+      dots: true,            
+      arrows: true,          
+      adaptiveHeight: true,  
+      prevArrow: '<button class="slick-prev" aria-label="Previous" type="button">Previous</button>',
+      nextArrow: '<button class="slick-next" aria-label="Next" type="button">Next</button>', 
+      responsive: [         
+      {
+        breakpoint: 768,   
+        settings: {
+          arrows: false   
         }
-      ]
-    });
+      }
+    ]
+  });
   
-    // Функционал для переключения слайдов при клике на точки
     $('.dot').click(function () {
       var index = $(this).index();
       $('.slider').slick('slickGoTo', index);
